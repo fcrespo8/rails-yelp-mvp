5ecf7855fb7c86a7fd10d834fceea50ca8682a40
Francisco Crespo <fcrespo8@hotmail.com>
2022-11-16 16:13:41 +0100

    finished 11

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,12 +9,18 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
 
     @review.restaurant = @restaurant
-    
+
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :restaurants do
-    resources :reviews
+    resources :reviews, only: [:new, :create]
   end
+
+  resources :reviews, only: [:destroy]
+
 end
